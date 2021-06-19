a916ff5d87b1de0ffe1c7b878832287b1563d514
jaegerwang <jaegerwang@tencent.com>
2021-06-19 18:40:12 +0800

    memtable/skiplist.go: 不再以接口实现 skiplist，直接使用 struct, 添加find方法.
    ISSUE: --issue=#1

--- memtable/skiplist.go
+++ memtable/skiplist.go
@@ -3,6 +3,7 @@ package memtable
 
 import (
 	"errors"
+	"math/rand"
 
 	"github.com/goleveldb/goleveldb/slice"
 )
@@ -10,14 +11,12 @@ import (
 // skiplist 最大高度.
 const maxHeight = 12
 
-// skiplist 定义跳表, 包含针对 Slice的 插入, 包含 操作.
-type skiplist interface {
-	insert(slice.Slice) error
-	contains(slice.Slice) bool
-}
+// seekRule 判断是否向后移动.
+// 返回 true 表示不满足最终条件, 需要继续移动.
+type seekRule func(target, next slice.Slice) bool
 
-// skiplistImpl 实现 skiplist 接口.
-type skiplistImpl struct {
+// skiplist 实现 skiplist 接口.
+type skiplist struct {
 	header *node
 }
 
@@ -28,16 +27,25 @@ type node struct {
 }
 
 // newSkiplist 实例化 skiplist 并返回.
-func newSkiplist() skiplist {
-	return &skiplistImpl{
+func newSkiplist() *skiplist {
+	return &skiplist{
 		header: &node{
 			next: [maxHeight]*node{},
 		},
 	}
 }
 
+// iterator 创建 skiplist 迭代器.
+// 创建出的迭代器初始位置为第一个节点.
+func (l *skiplist) iterator() *iterator {
+	it := &iterator{list: l}
+	it.seekToFirst()
+
+	return it
+}
+
 // insert 将 key 插入 skiplist, key 不可重复.
-func (l *skiplistImpl) insert(key slice.Slice) error {
+func (l *skiplist) insert(key slice.Slice) error {
 	var prevs [maxHeight]*node
 
 	cur := l.header
@@ -54,7 +62,7 @@ func (l *skiplistImpl) insert(key slice.Slice) error {
 
 	insertedNode := &node{key: key}
 
-	for level := 0; level < maxHeight; level++ {
+	for level := 0; level <= (rand.Int() % maxHeight); level++ {
 		insertedNode.next[level] = prevs[level].next[level]
 		prevs[level].next[level] = insertedNode
 	}
@@ -63,17 +71,37 @@ func (l *skiplistImpl) insert(key slice.Slice) error {
 }
 
 // contains 判断 key 是否存在于 skiplist 中.
-func (l *skiplistImpl) contains(key slice.Slice) bool {
+func (l *skiplist) contains(key slice.Slice) bool {
+	res := l.find(key, seekGreaterOrEqualRule)
+
+	return res != l.header && res.key.Compare(key) == slice.CMPSame
+}
+
+// find 寻找符合 rule 的节点.
+func (l *skiplist) find(target slice.Slice, rule seekRule) *node {
 	cur := l.header
 	for level := maxHeight - 1; level >= 0; level-- {
-		for cur.next[level] != nil && cur.next[level].key.Compare(key) != slice.CMPLarger {
+		for cur.next[level] != nil && rule(target, cur.next[level].key) {
 			cur = cur.next[level]
-		}
-
-		if cur.key.Compare(key) == slice.CMPSame {
-			return true
 		}
 	}
 
-	return false
+	return cur
+}
+
+// seekLessThanRule 获取小于 target 的规则.
+// 下一个小于target就继续移动.
+func seekLessThanRule(target, next slice.Slice) bool {
+	return next.Compare(target) == slice.CMPSmaller
+}
+
+// seekGreaterOrEqualRule 获取大于等于 target 的规则.
+// 下一个小于等于target就继续移动.
+func seekGreaterOrEqualRule(target, next slice.Slice) bool {
+	return next.Compare(target) != slice.CMPLarger
+}
+
+// seekLastRule 获取 skiplist 尾部的规则.
+func seekLastRule(target, next slice.Slice) bool {
+	return true
 }
